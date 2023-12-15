166db12ab61f2f3ae560eb0773c1b399071fc0d9
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-12-15 12:07:22 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import {
 
 export const App = () => {
   const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) ?? [
+    JSON.parse(localStorage.getItem('contacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -20,7 +20,7 @@ export const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const handleAddContact = newContact => {
